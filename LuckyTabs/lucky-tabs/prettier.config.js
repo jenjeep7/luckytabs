3ec09acebe710724af8e5d321423dd2cf2ce7326
Jennifer Johnson <jenjeep7@hotmail.com>
2025-08-03 08:32:13 -0500

    more fixes

--- LuckyTabs/lucky-tabs/prettier.config.js
+++ LuckyTabs/lucky-tabs/prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  semi: true,
+  semi: false,
   trailingComma: 'es5',
   singleQuote: true,
   printWidth: 80,
@@ -9,4 +9,6 @@ module.exports = {
   bracketSameLine: false,
   arrowParens: 'always',
   endOfLine: 'lf',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
 };
