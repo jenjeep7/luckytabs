f434ea6926310ec86f3763c9ffd7864fe61660f9
Jennifer Johnson <jenjeep7@hotmail.com>
2025-09-20 13:08:44 -0500

    trying a doover

--- LuckyTabs/lucky-tabs/ios/App/App/AppDelegate.swift
+++ LuckyTabs/lucky-tabs/ios/App/App/AppDelegate.swift
@@ -1,6 +1,5 @@
-import UIKit
-import Capacitor
-import FirebaseCore 
+
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,9 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        return true
+    FirebaseApp.configure()
+    // Override point for customization after application launch.
+    return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,17 +34,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
-
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
-    }
 
 }
